d566033edc9156ca6fb9ad41fda8fc8b5612bc11
cqiming <cqming2025@outlook.com>
2026-08-11 11:43:54 +0800

    fix admin bug

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './online_ide.css';
+import '../components/window.css';
 
 export default function Admin() {
   return (
-    <div style={{width: '100%', height: '100%', display: 'flex', flexDirection: 'column'}}>
+    <div className="window-content admin-container">
       <iframe
         title="管理平台"
         src="https://cqiming.pythonanywhere.com/admin"
-        style={{border: 'none', width: '100%', height: '100%'}}
+        className="admin-frame"
       />
     </div>
   );
